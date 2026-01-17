b653b4b96d23d8b58f396d4108890c9273ff5f17
Albox <albox7@gmail.com>
2026-01-17 23:44:16 +0100

    Aggiunto js esterno
    Attenzione: nella spiegazione dell'esercizio viene indicato di scrivere le variabili all'interno del tag script (non viene specificato di creare un js esterno e di collegarlo).

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+let num1 = 2;
+let num2 = 4;
+
+console.log(num1);
+console.log(num2);
+
+
+let stringa = 'Viva il weekend!';
+console.log(stringa);
+
+stringa = 'Il wekend è finito...';
+console.log(stringa);
+
+const PIGRECO = 3.14159265359;
+console.log(PIGRECO);
+
+PIGRECO = 12;
+console.log(PIGRECO);
+
